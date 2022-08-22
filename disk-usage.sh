6b9440f8501d9636f3586aab405ba71fee8b6548
nota-ru <nota-ru@yandex.ru>
2022-08-22 11:13:25 +0300

    seems like last var

--- disk-usage.sh
+++ disk-usage.sh
@@ -5,17 +5,17 @@ dir="$1"
 #проверка что введенный аргумент является директорией...
 if [ -d $dir ]; then
 	echo -e "cheking disk usage in $dir\n"
-
 else
 	echo -e "$dir is not a directory\n"
 	#пишем exit и некий отличный от нуля аргумент
 	#exit 1
 	exit 100
 
-
 fi
 
-
-top1=$(du /home/pyprep02 -d 1 2>/dev/null | sort -rn | head -n 1 | cut -f 2)
-disk=$(du /home/pyprep02 -d 1 2>/dev/null | sort -rn | head -n 1 | cut -f 1)
-
+echo "The heaviest subfolder of $dir is:"
+top1=$(du /home/notaru -d 1 2>/dev/null | sort -rn | head -n 2 | \
+         tail -n 1| cut -f 2)
+disk=$(du /home/notaru -d 1 2>/dev/null | sort -rn | head -n 2 | \
+         tail -n 1| cut -f 1)
+echo "$top1 with disk usage of $disk"                   
